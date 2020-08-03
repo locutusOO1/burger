f4b9d88cdada016bd7544eb2b211206ac5d8f376
Clark Kent with glasses <61812163+locutusOO1@users.noreply.github.com>
2020-08-03 08:07:11 -0500

    added heroku modified table to comments of schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -10,3 +10,13 @@ create table burgers (
     devoured boolean not null default false,
     primary key (id)
 );
+
+/*
+create table burgers (
+    id integer not null auto_increment,
+    burger_name varchar(50) not null,
+    devoured boolean not null default false,
+    createdAt timestamp not null default current_timestamp,
+    primary key (id)
+);
+*/
